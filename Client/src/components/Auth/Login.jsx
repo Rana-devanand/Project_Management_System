d2e51769dc37314ff8647d477ff0821a5233d4bd
Rana-devanand <gdsc.devanandrana@gmail.com>
2025-02-03 15:31:44 +0530

    Active the functionality of Login and registration

--- Client/src/components/Auth/Login.jsx
+++ Client/src/components/Auth/Login.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
+import axios from "axios";
 
 function Login() {
+  const [form, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/user/login`,
+        form
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div
-        className="min-h-screen flex items-start justify-between bg-cover bg-center "
+        className="min-h-screen flex flex-col md:flex-row items-center md:items-start justify-around md:justify-start md:my-auto bg-cover bg-center"
         style={{ backgroundImage: "url('/wave.jpg')" }}
       >
-        <div className="p-3 text-4xl">
-          <h1
-            className="font-['Roboto']"
-          >
-            Collab <span className="italic text-6xl font-bold text-cyan-500">P</span>ro
+        <div className="p-3 text-3xl md:text-4xl mt-4 md:mt-0 md:ml-8">
+          <h1 className="font-['Roboto']">
+            Collab&nbsp;
+            <span className="italic text-5xl md:text-6xl font-bold text-cyan-500">
+              P
+            </span>
+            ro
           </h1>
         </div>
-        <div className="w-full max-w-md my-auto p-8 mr-20 space-y-6 bg-white/80 backdrop-blur-sm rounded-xl shadow-lg">
-          <h2 className="text-3xl font-bold text-center text-gray-800">
+        <div className="w-full max-w-md mx-4 my-8 p-6 md:mx-auto md:my-auto md:mr-20 md:p-8 space-y-6 bg-white/80 backdrop-blur-sm rounded-xl shadow-lg">
+          <h2 className="text-2xl md:text-3xl font-bold text-center text-gray-800">
             Login
           </h2>
 
-          <form className="space-y-4">
-
+          <form className="space-y-4" onSubmit={HandleSubmit}>
             <div>
               <TextField
                 id="outlined-basic"
@@ -30,6 +54,8 @@ function Login() {
                 fullWidth
                 type="email"
                 required
+                name="Email"
+                onChange={handleChange}
               />
             </div>
 
@@ -41,6 +67,8 @@ function Login() {
                 type="password"
                 fullWidth
                 required
+                name="Password"
+                onChange={handleChange}
               />
             </div>
 
@@ -51,8 +79,14 @@ function Login() {
               Login
             </button>
           </form>
-          <div className="text-center text-sm text-gray-600">
-            Don't have an account? <a href="/register" className="text-indigo-600 hover:text-indigo-700">Register</a>
+          <div className="text-center text-xs md:text-sm text-gray-600">
+            Don't have an account?{" "}
+            <a
+              href="/register"
+              className="text-indigo-600 hover:text-indigo-700 text-sm md:text-base"
+            >
+              Register
+            </a>
           </div>
         </div>
       </div>
--- Client/src/components/Auth/Register.jsx
+++ Client/src/components/Auth/Register.jsx
@@ -1,7 +1,39 @@
-import React from "react";
-import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import axios from "axios";
 
 function Register() {
+  const [form, setFormValue] = useState({
+    Name: "",
+    Email: "",
+    Password: "",
+    Role: "",
+  });
+
+  const HandleChange = (e) => {
+    setFormValue({ ...form, [e.target.name]: e.target.value });
+  };
+
+  //localhost:5000/api/V1/user/register
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/user/register`,
+        form
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
       className="min-h-screen flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between bg-cover bg-center px-4 md:px-0"
@@ -13,19 +45,22 @@ function Register() {
           <span className="italic text-6xl font-bold text-cyan-500">P</span>ro
         </h1>
       </div>
-      <div className="w-full max-w-md my-8 md:my-auto p-6 md:p-8 mx-4 md:mr-20 space-y-6 bg-white/80 backdrop-blur-sm rounded-xl shadow-lg">
+      <div className="w-full max-w-md my-8 md:my-auto p-4 md:p-8 mx-4 md:mr-20 space-y-6 bg-white/80 backdrop-blur-sm rounded-xl shadow-lg">
         <h2 className="text-3xl font-bold text-center text-gray-800">
           Register
         </h2>
 
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={HandleSubmit}>
           <div>
             <TextField
+              type="text"
               id="outlined-basic"
               label="Name"
               variant="outlined"
               fullWidth
               required
+              name="Name"
+              onChange={HandleChange}
             />
           </div>
 
@@ -36,7 +71,9 @@ function Register() {
               variant="outlined"
               fullWidth
               type="email"
+              name="Email"
               required
+              onChange={HandleChange}
             />
           </div>
 
@@ -46,34 +83,60 @@ function Register() {
               label="Password"
               variant="outlined"
               type="password"
+              name="Password"
               fullWidth
               required
+              onChange={HandleChange}
             />
           </div>
 
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Role</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={form.Role}
+              label="Role"
+              name="Role"
+              onChange={HandleChange}
+            >
+              <MenuItem value={1}>Team leader</MenuItem>
+              <MenuItem value={2}>Project Manager</MenuItem>
+              <MenuItem value={3}>Employee</MenuItem>
+              <MenuItem value={4}>HR</MenuItem>
+            </Select>
+          </FormControl>
+
           <button
             type="submit"
-            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full mt-5 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Register
           </button>
           <p className="text-center text-sm text-gray-500">
-            Already have an account? &nbsp; 
+            Already have an account? &nbsp;
             <a href="/" className="text-indigo-600 hover:text-indigo-700">
               Login
             </a>
-           <br />
-            <a href="/forgot-password" className="text-indigo-600 hover:text-indigo-700">
+            <br />
+            <a
+              href="/forgot-password"
+              className="text-indigo-600 hover:text-indigo-700"
+            >
               Forgot Password?
             </a>
-            
             <br />
-            <a href="/terms-and-conditions" className="text-indigo-600 hover:text-indigo-700">
+            <a
+              href="/terms-and-conditions"
+              className="text-indigo-600 hover:text-indigo-700"
+            >
               Terms & Conditions
             </a>{" "}
-            (Coming Soon){" "}
-            <br />
-            <a href="/privacy-policy" className="text-indigo-600 hover:text-indigo-700">
+            (Coming Soon) <br />
+            <a
+              href="/privacy-policy"
+              className="text-indigo-600 hover:text-indigo-700"
+            >
               Privacy Policy
             </a>{" "}
             (Coming Soon){" "}
